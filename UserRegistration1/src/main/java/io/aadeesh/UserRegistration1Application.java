398e481289a215ccf5bdab4b254045adde45616e
Anitadevadiga <110710225+Anitadevadiga@users.noreply.github.com>
2022-08-27 16:13:19 +0530

    Update UserRegistration1Application.java

--- UserRegistration1/src/main/java/io/aadeesh/UserRegistration1Application.java
+++ UserRegistration1/src/main/java/io/aadeesh/UserRegistration1Application.java
@@ -2,8 +2,9 @@ package io.aadeesh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
+@ComponentScan(basepackages= {"io.aadeesh.controller;”})
 public class UserRegistration1Application 
 {
 	public static void main(String[] args) 
